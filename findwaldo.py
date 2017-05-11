5ec208671711e1c9194c528b71f95f1f94e971c3
majpark21 <marqui.thomas@yahoo.fr>
2017-05-11 17:52:33 +0200

    Skeleton of code

--- findwaldo.py
+++ findwaldo.py
@@ -10,12 +10,28 @@ Team Project
 Python Version: 3.6.1
 """
 
+"""
+- Need to build a library of patterns: one full, one torso, one head...
+- Return the position with the highest score in all arrays
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
+import skimage.transform
 
-def findwaldo(img):
+def findwaldo(img, template):
+    """
+    Returns the position of Waldo in image
+    :param img: path to image file
+    :param template: a numpy array (image) representing the researched pattern
+    :return: a 2-tuple corresponding to the position of Waldo
+    """
+    # 1) Read image
 
-    x = 42
-    y = 42
+    # 2) Gaussian pyramid img
+
+    # 3) Convolution with template
+
+    # 4) Noise removal?
 
     return x, y
