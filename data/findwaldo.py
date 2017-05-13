0136abdb24d487824721e709c3d39e02864981a7
majpark21 <marqui.thomas@yahoo.fr>
2017-05-13 15:14:13 +0200

    -

---
+++ data/findwaldo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Signal and Image Processing 2017 by Raphael Sznitman.
+Team Project
+- Livio Baetscher
+- Manuela Haefliger
+- Marc-Antoine Jacques
+Python Version: 3.6.1
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def findwaldo(img):
+
+    x = 42
+    y = 42
+
+return x, y
